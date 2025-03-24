7c74d5308eefb079fc7214523dc85326be91481b
christopherhess0 <83043287+christopherhess0@users.noreply.github.com>
2025-03-24 17:53:50 -0300

    feat: Implementación de página 404 con diseño moderno y animaciones

--- facturacion-afip/src/App.jsx
+++ facturacion-afip/src/App.jsx
@@ -1,22 +1,40 @@
-import { Provider } from 'react-redux';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import React from 'react';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
 import MainLayout from './layouts/MainLayout';
 import AdministrarConsorcios from './pages/administrarConsorcios/AdministrarConsorcios';
-import { store } from './redux/store';
+import Login from './pages/login/Login';
+import NotFound from './pages/notFound/NotFound';
 
-function App() {
+const App = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainLayout />}>
-            <Route path="administraciones" element={<AdministrarConsorcios />} />
-            {/* Aquí irán las demás rutas */}
-          </Route>
-        </Routes>
-      </Router>
-    </Provider>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <MainLayout>
+                <Navigate to="/administraciones" replace />
+              </MainLayout>
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/administraciones"
+          element={
+            <ProtectedRoute>
+              <MainLayout>
+                <AdministrarConsorcios />
+              </MainLayout>
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App; 
